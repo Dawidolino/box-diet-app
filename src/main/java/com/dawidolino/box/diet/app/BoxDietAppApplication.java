ae425bc642d41b033ed029d56a00d8992e0fdd91
Dawid Ruth <dawidolinp1@gmail.com>
2024-03-06 18:40:29 +0100

    Initial commit

---
+++ src/main/java/com/dawidolino/box/diet/app/BoxDietAppApplication.java
@@ -0,0 +1,13 @@
+package com.dawidolino.box.diet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxDietAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxDietAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawidolino/box/diet/app/BoxDietAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawidolino.box.diet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxDietAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
